696feb8eed784b65352dc644baf997cac14790bc
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-08-30 13:26:41 +0800

    Initial commit with cli-log.js

---
+++ cli-log.js
@@ -0,0 +1,66 @@
+/*:
+	@module-license:
+		The MIT License (MIT)
+
+		Copyright (c) 2014 Richeve Siodina Bebedor
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-module-license
+
+	@module-configuration:
+		{
+			"packageName": "cli-log",
+			"fileName": "cli-log.js",
+			"moduleName": "cliLog",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com:volkovasystems/cli-log.git",
+			"testCase": "cli-log-test.js",
+			"isGlobal": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+
+	@end-module-documentation
+
+	@include:
+	@end-include
+*/
+var cliLog = function cliLog( logLine, commandLineInterface, cliEnvironmentVariableSet ){
+	if( LOG_ON_COMMAND_PATTERN.test( logLine ) ){
+		cliEnvironmentVariableSet.LOG_STATE = true;
+		return;
+	}
+
+	if( LOG_OFF_COMMAND_PATTERN.test( logLine ) ){
+		cliEnvironmentVariableSet.LOG_STATE = false;
+		return;
+	}
+
+	if( cliEnvironmentVariableSet.LOG_STATE ){
+		console.log( logLine );
+		return;
+	}
+};
+
+const LOG_ON_COMMAND_PATTERN = /^\@log\:\s*[Oo][Nn]\s*$/;
+const LOG_OFF_COMMAND_PATTERN = /^\@log\:\s*[Oo][Ff]{2}\s*$/;
+
+module.exports = cliLog;
